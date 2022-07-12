f36119c5d22913873c251a123a58fe6ab5b60789
WriteCodeHaveFun <hit.like1@gmail.com>
2022-07-12 14:12:50 +0300

    added in-page navigation for destination.html (js+css)

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,36 @@
 let menuToggler = document.querySelector('[data-toggle-menu]');
+let inPageNav = document.querySelector('[data-in-page-nav]');
 
 menuToggler.addEventListener('click', toggleMenu);
+inPageNav.addEventListener('click', inPageNavigation)
 window.addEventListener('unload', clearInputCheckboxes);
 
 
 // functions for EventListeners
 function toggleMenu(e){
     let cssSelector = e.target.dataset.toggleMenu;
-    console.log(cssSelector);
     let elem = document.querySelector(`.${cssSelector}`);
     elem.classList.toggle('close');
+}
+
+function inPageNavigation(e){
+    if(!e.target.closest('li')) return;
+    if(e.target.closest('li').classList.contains('active')) return;
+    let navigationList = e.target.closest('ul').querySelectorAll('li');
+    let elemsToShow;
+
+    for(let i = 0; i < navigationList.length; i++){
+        navigationList[i].classList.remove('active');
+        if (navigationList[i] !== e.target.closest('li')) continue;
+        navigationList[i].classList.add('active');
+        elemsToShow = document.querySelectorAll(`[data-in-page-nav-id="${i}"]`);
+    }
+
+    if(!elemsToShow.length) return;
+    elemsToHide = document.querySelectorAll('[data-in-page-nav-id].show');
+    elemsToHide.forEach(elem => elem.classList.remove('show'));
+
+    elemsToShow.forEach(elem => elem.classList.add('show'));
 }
 
 function clearInputCheckboxes(){
